c6c9ab7fab74239ab4541488a21de509ca63aef0
meraki6966 <Mcclarin96@gmail.com>
2026-02-13 11:03:33 -0600

    Add logging function to output messages with timestamp

--- server/index.ts
+++ server/index.ts
@@ -31,5 +31,13 @@ export function log(message: string, source = "express") {
     second: "2-digit",
     hour12: true,
   });
+ export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
   console.log(`${formattedTime} [${source}] ${message}`);
 }
+
