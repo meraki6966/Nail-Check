eac9f6abb1784704182933b0f018e2357a3d723a
meraki6966 <Mcclarin96@gmail.com>
2026-03-17 14:55:27 -0500

    EMERGENCY: Fix double nav and contact page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -388,6 +388,7 @@ export default function Home() {
   };
 
   return (
+    <>
       <div className="max-w-7xl mx-auto px-4 py-8 space-y-16">
         
         {/* HEADER */}
@@ -660,6 +661,6 @@ export default function Home() {
           </div>
         </div>
       )}
-  
+    </>
   );
 }
